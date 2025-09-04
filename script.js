160bcf8c5d94cd7ebcb3a08d255e8d844b784b64
Raj Roy <royr938559@gmail.com>
2025-09-04 20:01:10 +0530

    Page 3 Added

--- script.js
+++ script.js
@@ -2,3 +2,30 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('main'),
     smooth: true
 });
+
+
+
+
+function page3Effect(){
+    let elemContainer = document.querySelector(".elem-container");
+let fixedImg = document.querySelector(".fixed-image");
+
+elemContainer.addEventListener("mouseenter", ()=>{
+    fixedImg.style.display="block";
+});
+
+elemContainer.addEventListener("mouseleave", ()=>{
+    fixedImg.style.display="none";
+});
+
+let elems = document.querySelectorAll(".featured-list");
+elems.forEach((el)=>{
+    el.addEventListener("mouseenter", ()=>{
+        let featuredImage = el.getAttribute("data-image")
+        fixedImg.style.backgroundImage = `url(${featuredImage})`;
+    })
+});
+}
+
+
+ page3Effect();
